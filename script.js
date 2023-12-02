4b285f9614f22f0474cc8b022403ff68dc368498
LochanaSamarasinghe <lochmadu378@gmail.com>
2023-12-02 22:48:32 +0530

    alignment feature

---
+++ script.js
@@ -0,0 +1,15 @@
+function f4() {
+    document.execCommand("justifyLeft", false, null);
+}
+
+function f5() {
+    document.execCommand("justifyCenter", false, null);
+}
+
+function f6() {
+    document.execCommand("justifyRight", false, null);
+}
+
+function f7() {
+    document.execCommand("justifyFull", false, null);
+}
